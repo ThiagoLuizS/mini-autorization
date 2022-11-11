2c40668878582dfe1151e90a28e1f4ae04e55734
Thiago Luiz <thiagoluiz_bh@hotmail.com>
2022-11-11 17:30:43 -0300

    commit init

---
+++ src/test/java/br/com/miniautorization/MiniAutorizationApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.miniautorization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniAutorizationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
